7a0ee9be3affa95d0983c8798507f141b0f63388
mhceg791-199 <mhceg791@gmail.com>
2026-03-18 11:29:08 +0200

    Fix ESM import issue

--- api/index.ts
+++ api/index.ts
@@ -3,4 +3,7 @@
 
 // export default handler;
 
-export { default } from "../src/server";
+// export { default } from "../src/server";
+
+import handler from "../src/server.js";
+export default handler;
